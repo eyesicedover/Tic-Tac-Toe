e734ac97d1a3d0b9fd61bba6d7266d0d8f2c994b
John Rykken and Stephanie Faber <stephanie@>
2018-01-25 15:08:53 -0800

    make it so additional pieces cannot be added to the board after win

--- js/scripts.js
+++ js/scripts.js
@@ -64,168 +64,185 @@ function showWin(z) {
 $(document).ready(function() {
   var player = 1;
   var newBoard = new Board();
-  console.log(newBoard);
   var didWin = false;
 
   // top left
   $(".tl").click(function() {
-    if (newBoard.tl === "") {
-      if (player === 1) {
-        $("#tl").text("X");
-        newBoard.tl = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#tl").text("O");
-        newBoard.tl = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+    if (didWin === false) {
+      if (newBoard.tl === "") {
+        if (player === 1) {
+          $("#tl").text("X");
+          newBoard.tl = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#tl").text("O");
+          newBoard.tl = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   }); // .tl click
 
   // top middle
-  $(".tm").click(function(){
-    if (newBoard.tm === "") {
-      if (player === 1) {
-        $("#tm").text("X");
-        newBoard.tm = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#tm").text("O");
-        newBoard.tm = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+  $(".tm").click(function() {
+    if (didWin === false) {
+      if (newBoard.tm === "") {
+        if (player === 1) {
+          $("#tm").text("X");
+          newBoard.tm = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#tm").text("O");
+          newBoard.tm = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   }); // .tm click
 
   // top right
-  $(".tr").click(function(){
-    if (newBoard.tr === "") {
-      if (player === 1) {
-        $("#tr").text("X");
-        newBoard.tr = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#tr").text("O");
-        newBoard.tr = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+  $(".tr").click(function() {
+    if (didWin === false) {
+      if (newBoard.tr === "") {
+        if (player === 1) {
+          $("#tr").text("X");
+          newBoard.tr = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#tr").text("O");
+          newBoard.tr = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   }); // .tm click
 
   // middle left
   $(".ml").click(function() {
-    if (newBoard.ml === "") {
-      if (player === 1) {
-        $("#ml").text("X");
-        newBoard.ml = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#ml").text("O");
-        newBoard.ml = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+    if (didWin === false) {
+      if (newBoard.ml === "") {
+        if (player === 1) {
+          $("#ml").text("X");
+          newBoard.ml = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#ml").text("O");
+          newBoard.ml = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   });
 
   // middle middle
   $(".mm").click(function() {
-    if (newBoard.mm === "") {
-      if (player === 1) {
-        $("#mm").text("X");
-        newBoard.mm = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#mm").text("O");
-        newBoard.mm = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+    if (didWin === false) {
+      if (newBoard.mm === "") {
+        if (player === 1) {
+          $("#mm").text("X");
+          newBoard.mm = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#mm").text("O");
+          newBoard.mm = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   });
 
   // middle right
   $(".mr").click(function() {
-    if (newBoard.mr === "") {
-      if (player === 1) {
-        $("#mr").text("X");
-        newBoard.mr = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#mr").text("O");
-        newBoard.mr = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+    if (didWin === false) {
+      if (newBoard.mr === "") {
+        if (player === 1) {
+          $("#mr").text("X");
+          newBoard.mr = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#mr").text("O");
+          newBoard.mr = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   });
 
   // bottom left
   $(".bl").click(function() {
-    if (newBoard.bl === "") {
-      if (player === 1) {
-        $("#bl").text("X");
-        newBoard.bl = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#bl").text("O");
-        newBoard.bl = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+    if (didWin === false) {
+      if (newBoard.bl === "") {
+        if (player === 1) {
+          $("#bl").text("X");
+          newBoard.bl = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#bl").text("O");
+          newBoard.bl = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   });
 
   // bottom middle
   $(".bm").click(function() {
-    if (newBoard.bm === "") {
-      if (player === 1) {
-        $("#bm").text("X");
-        newBoard.bm = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#bm").text("O");
-        newBoard.bm = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+    if (didWin === false) {
+      if (newBoard.bm === "") {
+        if (player === 1) {
+          $("#bm").text("X");
+          newBoard.bm = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#bm").text("O");
+          newBoard.bm = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   });
 
   // bottom right
   $(".br").click(function() {
-    if (newBoard.br === "") {
-      if (player === 1) {
-        $("#br").text("X");
-        newBoard.br = "X";
-        didWin = newBoard.win(player);
-        showWin(didWin);
-      } else {
-        $("#br").text("O");
-        newBoard.br = "O";
-        didWin = newBoard.win(player);
-        showWin(didWin);
+    if (didWin === false) {
+      if (newBoard.br === "") {
+        if (player === 1) {
+          $("#br").text("X");
+          newBoard.br = "X";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        } else {
+          $("#br").text("O");
+          newBoard.br = "O";
+          didWin = newBoard.win(player);
+          showWin(didWin);
+        }
+        player = playerTurn(player);
       }
-      player = playerTurn(player);
     }
   });
 
