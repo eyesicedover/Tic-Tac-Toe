4d51923198a60dd4be8b6aec2765cc9d1b5bef58
John Rykken and Stephanie Faber <john@>
2018-01-25 14:28:51 -0800

    Add reload button

--- js/scripts.js
+++ js/scripts.js
@@ -54,221 +54,280 @@ $(document).ready(function() {
   var player = 1;
   var newBoard = new Board();
   console.log(newBoard);
+  var didWin = false;
 
   // top left
   $(".tl").click(function() {
-    if (player === 1) {
-      $("#tl").text("X");
-      newBoard.tl = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.tl === "") {
+      if (player === 1) {
+        $("#tl").text("X");
+        newBoard.tl = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#tl").text("O");
+        newBoard.tl = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#tl").text("O");
-      newBoard.tl = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   }); // .tl click
 
   // top middle
   $(".tm").click(function(){
-    if (player === 1) {
-      $("#tm").text("X");
-      newBoard.tm = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.tm === "") {
+      if (player === 1) {
+        $("#tm").text("X");
+        newBoard.tm = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#tm").text("O");
+        newBoard.tm = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#tm").text("O");
-      newBoard.tm = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   }); // .tm click
 
   // top right
   $(".tr").click(function(){
-    if (player === 1) {
-      $("#tr").text("X");
-      newBoard.tr = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.tr === "") {
+      if (player === 1) {
+        $("#tr").text("X");
+        newBoard.tr = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#tr").text("O");
+        newBoard.tr = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#tr").text("O");
-      newBoard.tr = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   }); // .tm click
 
   // middle left
   $(".ml").click(function() {
-    if (player === 1) {
-      $("#ml").text("X");
-      newBoard.ml = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.ml === "") {
+      if (player === 1) {
+        $("#ml").text("X");
+        newBoard.ml = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#ml").text("O");
+        newBoard.ml = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#ml").text("O");
-      newBoard.ml = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   });
 
   // middle middle
   $(".mm").click(function() {
-    if (player === 1) {
-      $("#mm").text("X");
-      newBoard.mm = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.mm === "") {
+      if (player === 1) {
+        $("#mm").text("X");
+        newBoard.mm = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#mm").text("O");
+        newBoard.mm = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#mm").text("O");
-      newBoard.mm = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   });
 
   // middle right
   $(".mr").click(function() {
-    if (player === 1) {
-      $("#mr").text("X");
-      newBoard.mr = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.mr === "") {
+      if (player === 1) {
+        $("#mr").text("X");
+        newBoard.mr = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#mr").text("O");
+        newBoard.mr = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#mr").text("O");
-      newBoard.mr = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   });
 
   // bottom left
   $(".bl").click(function() {
-    if (player === 1) {
-      $("#bl").text("X");
-      newBoard.bl = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.bl === "") {
+      if (player === 1) {
+        $("#bl").text("X");
+        newBoard.bl = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#bl").text("O");
+        newBoard.bl = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#bl").text("O");
-      newBoard.bl = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   });
 
   // bottom middle
   $(".bm").click(function() {
-    if (player === 1) {
-      $("#bm").text("X");
-      newBoard.bm = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.bm === "") {
+      if (player === 1) {
+        $("#bm").text("X");
+        newBoard.bm = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#bm").text("O");
+        newBoard.bm = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#bm").text("O");
-      newBoard.bm = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
   });
 
   // bottom right
   $(".br").click(function() {
-    if (player === 1) {
-      $("#br").text("X");
-      newBoard.br = "X";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 1 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
+    if (newBoard.br === "") {
+      if (player === 1) {
+        $("#br").text("X");
+        newBoard.br = "X";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 1 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
+      } else {
+        $("#br").text("O");
+        newBoard.br = "O";
+        didWin = newBoard.win(player);
+        if (didWin === true) {
+          $(".winText").text("Player 2 Wins!");
+          $("#playAgain").show();
+        } else if (didWin === "cat") {
+          $(".winText").text("Cat's Game!");
+          $("#playAgain").show();
+        }
       }
-    } else {
-      $("#br").text("O");
-      newBoard.br = "O";
-      var didWin = newBoard.win(player);
-      if (didWin === true) {
-        $(".winText").text("Player 2 Wins!");
-      } else if (didWin === "cat") {
-        $(".winText").text("Cat's Game!");
-      }
+      player = playerTurn(player);
     }
-    player = playerTurn(player);
+  });
+
+  $("button").click(function(){
+    location.reload();
   });
 
 
